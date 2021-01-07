eaa20529b89d7dae32408d768680384a98da1ba0
Haisheng.Wu.mac <freizl@gmail.com>
2021-01-07 09:55:16 -0800

    add code

--- Main.hs
+++ Main.hs
@@ -3,8 +3,10 @@ module Main where
 import Euterpea
 
 
+x1 = c 4 en :+: g 4 en :+: c 5 en :+: g 5 en
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  play $ c 4 qn
+  -- writeMidi "test.mid" x1
+  playDev 1 x1
